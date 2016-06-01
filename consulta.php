d2be17694e4e8a5ef2e6460d388d868fe9bc771d
TommyPozo <tomaspozo@outlook.com>
2016-06-01 12:08:55 -0500

    5

---
+++ consulta.php
@@ -0,0 +1,31 @@
+<?php
+
+$host='localhost';
+$db='postgres';
+$user='postgres';
+$pass='lucas2';
+
+try
+{
+   $conn=new PDO("pgsql:dbname=$db;host=$host", $user, $pass);
+}
+catch(PDOException $pe)
+{
+    die('Couldnt connect to the database beacuse: '. $pe->getMessage());
+}
+
+$q=$conn->query('select * from test');
+
+if (!$q)
+{
+$ei=$conn->errorInfo();
+die('Couldnt execute query because:'. $ei[2]);
+}
+
+while($r=$q->fetch(PDO::FETCH_ASSOC))
+{
+	echo $r['id'];
+	echo $r['name'];
+}
+
+?>
